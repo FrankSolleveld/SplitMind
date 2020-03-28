a40be8311527aadc1261b289d98c51298f7f8062
IAmMisterFrank <fsolleveld@outlook.com>
2020-03-28 15:32:11 +0100

    Initial commit

--- Split Mind/ARViewController.swift
+++ Split Mind/ARViewController.swift
@@ -6,19 +6,20 @@
 //  Copyright © 2020 Frank Solleveld. All rights reserved.
 //
 
+// This will be the AR View Controller.
+
 import UIKit
 import RealityKit
 
-class ViewController: UIViewController {
+class ARViewController: UIViewController {
     
     @IBOutlet var arView: ARView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         // Load the "Box" scene from the "Experience" Reality File
         let boxAnchor = try! Experience.loadBox()
-        
+
         // Add the box anchor to the scene
         arView.scene.anchors.append(boxAnchor)
     }
