58f1dbbdb941d62682a50655e27b4eff8d94b72e
IAmMisterFrank <fsolleveld@outlook.com>
2020-03-28 13:13:30 +0100

    Initial Commit

---
+++ Split Mind/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Split Mind
+//
+//  Created by Frank Solleveld on 28/03/2020.
+//  Copyright © 2020 Frank Solleveld. All rights reserved.
+//
+
+import UIKit
+import RealityKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var arView: ARView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+    }
+}
